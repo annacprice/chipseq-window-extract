8463f45c18fbc5b92754082c68b7a1795f6d9381
Anna Price <36504362+annacprice@users.noreply.github.com>
2019-10-22 17:16:58 +0100

    added parser for ref genome

--- chipWinExtract.py
+++ chipWinExtract.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
 import pandas as pd
+import re
+from Bio import SeqIO
+import numpy as np
 
 def TSVextract():
+    # extract columns from the tsv
 
     # load tsv
     tsv = pd.read_csv('testdata/chipseq.tsv', sep='\t')
@@ -12,8 +16,23 @@ def TSVextract():
     startCoord = tsv['start'].values
     endCoord = tsv['end'].values
 
+    return seqName, startCoord, endCoord
+
+def genomeArray():
+    # save the reference genome to an array
+
+    genome = []
+    for seq_rec in SeqIO.parse('testdata/genome.fasta', '''fasta'''):
+        genome.append(seq_rec.seq)
+
+    genomeArr = np.array(genome)
+
+    return genomeArr
+
 def main():
-    TSVextract()
+    seqName, startCoord, endCoord = TSVextract()
+    genomeArr = genomeArray()
+
 
 if __name__ == "__main__":
     main()
